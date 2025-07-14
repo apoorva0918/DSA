63382eac922bb217bfa18865b8b36f895f23f868
Apoorva Raj <apoorvaaa81@gmail.com>
2025-07-14 22:41:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    bool areAnagrams(string& s1, string& s2) {
+        if(s1.length()!=s2.length()) return false;
+        
+        int freq[26]={0};
+        
+        for(int i=0; i<s1.length(); i++){
+            freq[s1[i]-'a']++;
+        }
+        
+        for(int i=0; i<s2.length(); i++){
+            freq[s2[i]-'a']--;
+        }
+        
+        for(int i=0; i<26; i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
